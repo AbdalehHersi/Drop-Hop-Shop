09f282fbd96b3f5a372f8a99ba20f9d858d1c10a
Abdaleh Hersi <therealhersi03@gmail.com>
2022-10-03 13:09:11 +0100

    Completed Tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -47,6 +47,21 @@ router.post('/', async(req, res) => {
 
 router.put('/:id', async(req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  });
+  if (!tagData) {
+    res.status(400).json({message: "No tag by this ID found"});
+    return;
+  }
+  res.status(200).json(tagData);
+  } catch (error) {
+  res.status(500).json(error)    
+  }
+  
 });
 
 router.delete('/:id', async(req, res) => {
